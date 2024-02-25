6779455681aef4acb7e75ef2111df0f20bfae0a3
Minh Pham <126002737+iphaminh@users.noreply.github.com>
2024-02-25 16:04:29 -0500

    modify hook with useWindowSize for Navbar plus adding css too

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,45 +1,41 @@
-// src/components/CustomNavbar.js
-import React from "react";
-import {
-  Navbar,
-  NavbarContent,
-  NavbarItem,
-  Link,
-} from "@nextui-org/react";
+import React, { useState } from "react";
+import { Navbar, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
 import PhotonLogo from "../PhotonLogo";
-import "./CustomNavbar.css"; // Make sure this import is correct
+import "./CustomNavbar.css";
+import { useWindowSize } from "../../hooks/useWindowSize";
 
 const CustomNavbar = () => {
+  const { width } = useWindowSize();
+  const [isMenuVisible, setIsMenuVisible] = useState(false); // Added state to manage mobile menu visibility
+  const isMobile = width < 768; // Example breakpoint for mobile devices
+
   return (
     <Navbar isBordered variant="sticky" className="custom-navbar">
-      <NavbarContent className="navbar-content">
-        <div className="navbar-section">
-          <NavbarItem className="navbar-item">
-            <Link href="#">Home</Link>
-          </NavbarItem>
-          <NavbarItem className="navbar-item">
-            <Link href="#">Cine</Link>
-          </NavbarItem>
-          <NavbarItem className="navbar-item">
-            <Link href="#">Foto</Link>
-          </NavbarItem>
-        </div>
-
-        <div className="logo-section">
+      <NavbarContent className={`navbar-content ${isMobile ? 'mobile' : ''}`}>
+        {isMobile && (
+         <div className="hamburger-menu" onClick={() => setIsMenuVisible(!isMenuVisible)}>
+         <div className="bar"></div>
+         <div className="bar"></div>
+         <div className="bar"></div>
+       </div>       
+        )}
+        {(!isMobile || isMenuVisible) && (
+          <>
+            <div className="navbar-section">
+              <NavbarItem className="navbar-item"><Link href="#">Home</Link></NavbarItem>
+              <NavbarItem className="navbar-item"><Link href="#">Cine</Link></NavbarItem>
+              <NavbarItem className="navbar-item"><Link href="#">Foto</Link></NavbarItem>
+            </div>
+            <div className="logo-section">
           <PhotonLogo />
         </div>
-
-        <div className="navbar-section">
-          <NavbarItem className="navbar-item">
-            <Link href="#">Pricing</Link>
-          </NavbarItem>
-          <NavbarItem className="navbar-item">
-            <Link href="#">Contact</Link>
-          </NavbarItem>
-          <NavbarItem className="navbar-item">
-            <Link href="#">Testimonial</Link>
-          </NavbarItem>
-        </div>
+            <div className="navbar-section">
+              <NavbarItem className="navbar-item"><Link href="#">Pricing</Link></NavbarItem>
+              <NavbarItem className="navbar-item"><Link href="#">Contact</Link></NavbarItem>
+              <NavbarItem className="navbar-item"><Link href="#">Testimonial</Link></NavbarItem>
+            </div>
+          </>
+        )}
       </NavbarContent>
     </Navbar>
   );
