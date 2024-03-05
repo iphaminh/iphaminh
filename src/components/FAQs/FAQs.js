acaeeec2e60fd8d0a1c7eb2445be609ff8223a45
Minh Pham <minhmeoquay@hotmail.com>
2024-03-04 23:29:24 -0500

    finish the FAQs Components with css and js

---
+++ src/components/FAQs/FAQs.js
@@ -0,0 +1,86 @@
+// src/components/FAQs/FAQs.js
+
+import React from 'react';
+import './FAQs.css'; // Your CSS file for styling
+
+const FAQs = () => {
+    const faqs = [
+    {
+      question: '• WHAT IS THE TURN AROUND TIME?',
+      answer: 'According to my contract, the production and distribution of all wedding and elopement videos will span 6-8 weeks. Frequently, I share highlights and sneak peeks on my social media platforms, so stay tuned for glimpses of your special day',
+    },
+
+    {
+      question: '• DO YOU OFFER DISCOUNTS?',
+      answer: 'Yes! I give a 10% discount on the package price to both my returning clients and military members...',
+    },
+
+    {
+      question: '• DOES THE DEPOSIT COME OUT OF THE PACKAGE PRICE?',
+      answer: 'Yes! The 30% deposit is part of your package price, it is not an additional cost. However, it is non-refundable in order to hold your date.',
+    },
+
+    {
+      question: 'CAN I CHOOSE MY MUSIC?',
+      answer: 'I typically select music that complements your film is mood, ensuring all tracks are properly licensed from reputable sources like Artlist, Musicbed, or Audio Jungle. Licensing is crucial to avoid legal issues and content takedowns. Artlist tracks are included at no extra cost due to my annual subscription. If you prefer to pick your own song from Musicbed or Audio Jungle, note that it will incur an additional fee of $25-50 per song. Please express your preference for selecting music before signing the contract, and feel free to discuss any questions during the booking process.',
+    },
+
+    {
+      question: '• DOES DRONE FOOTAGE COST EXTRA?',
+      answer: 'Absolutely not, I can capture stunning aerial footage for your wedding film. However, it is essential to confirm with your venue coordinator that drone flights are permitted at your event location. While I prioritize public safety and will not operate the drone in poor weather conditions, under favorable circumstances, we are all set to take to the skies!',
+    },
+ 
+    {
+      question: '• DO YOU TRAVEL FOR FILMS?',
+      answer: 'Absolutely! While I am based in Arkansas and Georgia, I frequently travel nationwide for weddings and elopements. For information on my out-of-state packages, including estimated travel costs, please feel free to reach out to me',
+    },
+
+    {
+      question: '• HOW DO I DOWNLOAD MY VIDEO?',
+      answer: 'I provide all my clients with a Google Drive link to their final video edit. Additionally, you can access your video through my Vimeo webpage and download it directly. For convenient mobile access and sharing, download the Vimeo app on your phone and save your wedding video straight to your device.',
+    },
+
+    {
+      question: '• HOW LONG HAVE YOU BEEN FILMING WEDDINGS?',
+      answer: 'I have been capturing the essence of weddings through film since 2015, after graduating with a degree in Filmmaking from UCA. My work has not only earned me the Best Cinematography award in Conway, AR, but it has also been featured in numerous industry-leading wedding pages',
+    },
+
+    {
+      question: '• WHEN SHOULD I REACH OUT TO BOOK?',
+      answer: 'Reach out as soon as you can – it is always the right time to get your wedding date on my calendar! I limit my bookings to 20-25 weddings annually to ensure each film gets the time and devotion it deserves, from planning and filming to editing. I often start booking 1-2 years ahead, with weekend dates (especially Saturdays) during the summer and fall filling up the fastest. Looking forward to creating something beautiful with you',
+    },
+
+    {
+      question: '• ARE TAXES INCLUDED?',
+      answer: 'Arkansas State and Georgia State Sales Tax is 7% of your total package price. This amount will be added to your client invoice in compliance with Arkansas State Law.',
+    },
+  ];
+
+  const halfwayIndex = Math.ceil(faqs.length / 2);
+  const firstColumnFaqs = faqs.slice(0, halfwayIndex);
+  const secondColumnFaqs = faqs.slice(halfwayIndex);
+
+  return (
+    <div className="faqs-container">
+      <div className="faqs-column">
+        {firstColumnFaqs.map((faq, index) => (
+          <div key={index} className="faq-item">
+            <h3 className="faq-question">{faq.question}</h3>
+            <p className="faq-answer">{faq.answer}</p>
+          </div>
+        ))}
+      </div>
+      <div className="faqs-divider"></div> {/* This is the vertical line */}
+      <div className="faqs-column">
+        {secondColumnFaqs.map((faq, index) => (
+          <div key={index} className="faq-item">
+            <h3 className="faq-question">{faq.question}</h3>
+            <p className="faq-answer">{faq.answer}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FAQs;
--- src/pages/PricingPage/Pricing.js
+++ src/pages/PricingPage/Pricing.js
@@ -1,58 +1,62 @@
 // src/pages/PricingPage/Pricing.js
 import React from 'react';
 import './Pricing.css'; // Make sure the path to your CSS file is correct
+import FAQs from '../../components/FAQs/FAQs';
 
 export default function Pricing() {
   return (
-    <div>
-  
-      
-      <main className="container mx-auto px-4">
-        {/* Hero section with headline */}
-        <section className="text-center py-12">
-  <div className="responsive-img-wrapper">
-    <img src="/assets/pricing/elegant-wedding-table-setting-georgia-phaminh.png" alt="Elegant Wedding Table Setting" />
-  </div>
-</section>
-
-
-        
-        {/* Pricing Info */}
-        <div className="text-center py-12 px-4">
-    <h1 className="text-4xl mb-4 font-montserrat">Preserving Life's Priceless Moments Through The Art Of Film</h1>
-    <p className="mb-8 font-montserrat" style={{ fontSize: '12px' }}>VIDEO PRODUCTION IS A UNIQUE WAY TO REPLAY SPECIAL MOMENTS IN OUR LIVES. WHETHER IT BE THE CEREMONIAL KISS UNDER THE ARBOR FOR THE FIRST TIME AS HUSBAND
+ <div className="container mx-auto px-4">
+   {/* Hero section with headline */}
+   <section className="text-center py-12">
+        <div className="responsive-img-wrapper">
+          <img src="/assets/pricing/elegant-wedding-table-setting-georgia-phaminh.png" alt="Elegant Wedding Table Setting" />
+        </div>
+        <h1 className="hero-title">
+            Preserving Life's Priceless Moments <br /> Through The Art Of Film
+          </h1>   
+          <p className="hero-text">VIDEO PRODUCTION IS A UNIQUE WAY TO REPLAY SPECIAL MOMENTS IN OUR LIVES. WHETHER IT BE THE CEREMONIAL KISS UNDER THE ARBOR FOR THE FIRST TIME AS HUSBAND
 AND WIFE; YOUR SWEET GRANDPA ROCKING OUT ON THE DANCE FLOOR; A NEWBORN CHILD BEING BROUGHT INTO THIS WORLD; ONCE IN A LIFETIME MEMORIES AND ADVENTURES
 ARE ABLE TO BE RELIVED THROUGH THE ESSENCE OF FILM. JUST PICTURE THIRTY YEARS FROM NOW, HAVING THE ABILITY TO GO BACK AND RELIVE YOUR FATHER WALKING YOU
 DOWN THE AISLE ON YOUR WEDDING DAY OR THE FIRST TIME SEEING YOUR LOVED ONE COMING HOME TO YOU, AFTER YEARS OF BEING APART.</p>
-        
+
+  </section>
+
         {/* Packages */}
-        <div className="flex flex-col md:flex-row justify-around items-start md:items-center my-12 mx-auto">
+        <div className="packages">
       {/* Package I */}
-      <div className="package-info text-center mx-4 mb-8 md:mb-0">
-        <h2 className="font-montserrat">Elopement Videography Packages</h2>
-        <p className="font-crimson">Intimate Elopement Packages for the Adventurous Couple. Ready to break away from tradition and begin your journey together in a breathtaking, dream location? This package is crafted for you!</p>
-        <p className="package-price">Starting at $2,500</p>          </div>
+          <div className="package">
+        <h2 className="package-title">Elopement Videography Packages</h2>
+        <p className="package-description">Intimate Elopement Packages for the Adventurous Couple. Ready to break away from tradition and begin your journey together in a breathtaking, dream location? This package is crafted for you!</p>
+        <p className="package-price">Starting at $2,500</p>         
+          </div>
 
           {/* Package II */}
-          <div className="package-info text-center mx-4 mb-8 md:mb-0">
-          <h2 className="font-montserrat">Wedding Videography Packages</h2>
-          <p className="font-crimson">Wedding Packages Tailored for Every Bride, from the Minimalist to the Extravagant. Our custom options ensure every precious moment, from dawn till dusk, is captured beautifully on your special day.</p>
-            <p className="package-price">Starting at $3,000</p>
+          <div className="package">
+        <h2 className="package-title">Wedding Videography Packages</h2>
+        <p className="package-description">Wedding Packages Tailored for Every Bride, from the Minimalist to the Extravagant. Our custom options ensure every precious moment, from dawn till dusk, is captured beautifully on your special day.</p>
+        <p className="package-price">Starting at $2,700</p>
           </div>
 
           {/* Package III */}
-          <div className="package-info text-center mx-4 mb-8 md:mb-0">
-          <h2 className="font-montserrat">First Look Videography Packages</h2>
-          <p className="font-crimson">Embark on a Romantic Adventure with Your Love, either before or after your wedding. Seize the chance to dress up and explore a stunning locale for an intimate first look, vow renewal, or a serene escapade, just for the two of you.</p>
-            <p className="package-price">Starting at $500</p>
+          <div className="package">
+        <h2 className="package-title">First Look Videography Packages</h2>
+        <p className="package-description">Embark on a Romantic Adventure with Your Love, either before or after your wedding. Seize the chance to dress up and explore a stunning locale for an intimate first look, vow renewal, or a serene escapade, just for the two of you.</p>
+        <p className="package-price">Starting at $500</p>
           </div>
         </div>
+         {/* New Section Below Packages */}
+      <section className="call-to-action-section">
+        <img src="/assets/pricing/georgia-wedding-couple-portraits-phaminh.png" alt="Georgia Wedding Couple" className="w-full" />
+        <div className="cta-content">
+          <h2 className="cta-heading">LET'S DO THIS THING!</h2>
+          <p className="cta-text">INQUIRE ABOUT YOUR WEDDING DATE</p>
+          <button className="cta-button">GET IN TOUCH</button>
+        </div>
+      </section>
+
+      <FAQs />
+
       </div>
-          
-          {/* Pricing option 2 */}
-          {/* ... similar structure for other pricing options */}
-      </main>
-      
-    </div>
+    
   );
 }
