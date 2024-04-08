0eab33e0b167bae4bb069b4e9c369195c6c65539
Minh Pham <126002737+iphaminh@users.noreply.github.com>
2024-04-08 16:53:23 -0400

    update efffect for about me component

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import './AboutMe.css'; // This imports regular CSS file
+import React, { useState, useEffect, useRef } from 'react';
+import './AboutMe.css';
 
 const AboutMe = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [isAnimated, setIsAnimated] = useState(false);
+  const aboutMeRef = useRef();
+
+  const handleScroll = () => {
+    if (aboutMeRef.current) {
+      const top = aboutMeRef.current.getBoundingClientRect().top;
+      const onScreen = top < window.innerHeight && top > 0;
+      
+      // Set the section as visible only if it hasn't been animated yet
+      if (onScreen && !isAnimated) {
+        setIsVisible(true);
+        setIsAnimated(true); // Prevent further animations
+      }
+
+      // Reset the animation if the section is no longer visible
+      if (!onScreen && isAnimated) {
+        setIsVisible(false);
+        setIsAnimated(false); // Allow animation to trigger again
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [isAnimated]); // Dependency array includes isAnimated to re-add event listener
+
+
   return (
-    <div className="aboutMeContainer">
-      <div className="aboutMeContent">
-        <h2>I 'm MINH!</h2>
+    <div className="aboutMeContainer" ref={aboutMeRef}>
+      <div className={`aboutMeContent ${isVisible ? 'slide-in' : ''}`}>
+        <h2>I'm MINH!</h2>
         <p>
           Passionate about capturing life's moments, I specialize in turning
           visions into cinematic stories. Away from the camera, I'm exploring
@@ -13,7 +45,7 @@ const AboutMe = () => {
           your love story through my lens.
         </p>
       </div>
-      <div className="aboutMeImage">
+      <div className={`aboutMeImage ${isVisible ? 'fade-in' : ''}`}>
         <img src="/assets/images/phaminh-cinematography.png" alt="Cinematographer MINH in natural setting" />
       </div>
     </div>
